1bbb4ded09406248c521edd6c38606a77ed85410
Gerald Bauer <gerald.bauer@gmail.com>
2024-02-11 20:55:37 +0100

    up

--- generate_pigs.rb
+++ generate_pigs.rb
@@ -76,11 +76,67 @@ specs = parse_data( <<DATA )
    pig (aa), red_shemagh_agal (pig)
    pig (aa), red_shemagh (pig), regular shades
    pig (aa), white_shemagh_gold_agal (pig), laser eyes gold
+
+   pig gold (a), regular shades, red mohawk (pig) 
+   pig green (a), 3d glasses
+
+   pig alien (aa), cap 
+   pig alien (aa), cap forward, pipe
+   pig green (a), laser eyes gold
+ 
+   pig blue (a), vr pro
+   pig green (a), clown eyes green
+   pig green (aa), cap mcd
+
+   pig alien (aaa), hoodie (pig)
+   pig orc (aaa), hoodie light (pig)
+   pig pink (aaa), hoodie dark (pig)
+
+   pig alien (aaa), hood (pig)
+   pig gold (aaa), hood dark (pig)
+  
+   pig green (aaa), clown eyes green, hoodie (pig)
+   pig gold (aaa), hoodie light (pig), laser eyes gold
+   pig alien red (a), vr pro
+   pig pink (aaa), hoodie dark (pig), vr pro
+
+   pig pink (a), blonde bob (pig)
+   pig pink (a), blonde bob (pig), big shades
+   pig pink (a), blonde bob (pig), laser eyes blue
+   pig pink (a), blonde bob (pig), mole, gold chain
+   pig green (a), blonde bob (pig), mole, blue eye shadow, gold chain
+   
+
+   pig gold (a), crazy hair (pig) 
+   pig green (a), crazy hair (pig), nerd glasses
+
+   pig green (a), wild blonde (pig)
+   pig gold (a), wild blonde (pig), classic shades
+   pig alien red (a), blonde side (pig)
+   pig pink (a), blonde side (pig), flowers
+   pig blue (a), orange side (pig)
+   pig blue (a), orange side (pig), choker, purple eye shadow
+
+   pig green (a), heart shades, red mohawk (pig) 
+  
+   pig orc (aa), cap mcd white
+   pig alien (aa), cap mcd flipped
+   pig gold (aa), cap mcd black
+   pig green (aa), cap subway, horned rim glasses
+   pig blue (aa), cap burger king, regular shades
+  
+   pig orc (a), vr pro, gold chain
+   pig orc (aa), bandana, eye patch
+
+   pig alien red (aa), brown_shemagh_agal (pig)
+   pig blue (aa), red_shemagh_agal (pig)
+   pig green (aa), red_shemagh (pig), regular shades
+   pig gold (aa), white_shemagh_gold_agal (pig), laser eyes gold
 DATA
 
 
 
-cols = 7
+cols = 14
 rows = specs.size / cols 
 rows += 1    if specs.size % cols != 0
 
